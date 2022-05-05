fd332d70334d976da04d713f141a10a0070aae7f
Lucy Toman <lucy@toman.me.uk>
2022-05-05 13:12:41 +0200

    adds method checker

---
+++ pages/api/comments/[id].js
@@ -0,0 +1,15 @@
+import comments from "../../../comments.json";
+
+export default function handler(req, res) {
+    switch (req.method) {
+        case "GET":
+            res.status(200).json({ post: req.query.id, comments });
+            break;
+        case "POST":
+            //...handle the POST request here
+            break;
+        default:
+            res.status(405).end();
+            break;
+    }
+}
